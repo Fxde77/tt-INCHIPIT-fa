3368fceb518e098dce60112f604dcb287834a315
Fxde77 <trincherofederico8@gmail.com>
2026-02-11 12:02:14 +0100

    Refactor fa_module syntax and comments

--- src/fa.sv
+++ src/fa.sv
@@ -1,30 +1,31 @@
 //  Module: fa
-//
 
 module fa_module
-    (
-        input  logic A,
-        input  logic B,
-        input  logic Cin,
-        output logic S,
-        output logic Cout
-    );
+(
+    input  logic A,
+    input  logic B,
+    input  logic Cin,
+    output logic S,
+    output logic Cout
+);
 
-    logic i0, i1, i2;
+    logic i0;  // A xor B
+    logic i1;  // A and B
+    logic i2;  // Cin and (A xor B)
 
     // Primo XOR
     xor u0(i0, A, B);
 
-    // Somma finale
+    // Secondo XOR -> Somma
     xor u1(S, i0, Cin);
 
     // Primo AND
     and u2(i1, A, B);
 
     // Secondo AND
-    and u3(i2, i0, Cin);
+    and u3(i2, Cin, i0);
 
-    // OR finale per il carry out
+    // OR finale -> Carry out
     or u4(Cout, i1, i2);
 
-endmodule: fa_module
+endmodule : fa_module
